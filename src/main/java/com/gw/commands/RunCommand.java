595f80734ad5f723de22dd0e3223458411d38d5a
Ahmed Aln(u)aim <ahmed.y.alnaim@gmail.com>
2022-06-28 10:47:26 -0400

    latest changes to run comand CLI for workflow

--- src/main/java/com/gw/commands/RunCommand.java
+++ src/main/java/com/gw/commands/RunCommand.java
@@ -2,6 +2,7 @@ package com.gw.commands;
 
 import com.gw.tools.WorkflowTool;
 import com.gw.utils.BaseTool;
+import com.gw.utils.BeanTool;
 import com.gw.utils.RandomString;
 
 import org.springframework.stereotype.Component;
@@ -14,7 +15,7 @@ import picocli.CommandLine.Parameters;
 @Command(name = "run")
 public class RunCommand implements Runnable {
 
-    @Parameters(index = "0", description = "workflow id to run")
+    @Option(names = { "-w", "--workflow" }, description = "workflow id to run")
     String workflowId;
 
     @Option(names = { "-h", "--hosts" }, description = "hosts to run on")
@@ -23,8 +24,11 @@ public class RunCommand implements Runnable {
     @Option(names = { "-e", "--environments" }, description = "environments to run on")
     String[] envs;
 
-    @Option(names = { "-w", "--workflow" }, description = "workflow package or path to workflow.json to run")
-    String workflowZipOrPathToJson;
+    // @Option(names = { "-w", "--workflow" }, description = "workflow package or path to workflow.json to run")
+    // String workflowZipOrPathToJson;
+
+    @Option(names = { "-p", "--password" }, description = "passowrd of desired host to run on")
+    String[] password;
 
     @Option(names = { "--history" }, description = "workflow history id to run", required = false, defaultValue = "")
     String historyId;
@@ -32,14 +36,15 @@ public class RunCommand implements Runnable {
     public void run() {
         System.out.println("running run command with workflow id" + workflowId);
         // TODO: implement
-        if (workflowZipOrPathToJson != null) {
-            System.out.println("workflow zip or path to json: " + workflowZipOrPathToJson);
-        }
-        BaseTool bt = new BaseTool();
-        WorkflowTool wt = new WorkflowTool();
+        // if (workflowZipOrPathToJson != null) {
+        //     System.out.println("workflow zip or path to json: " + workflowZipOrPathToJson);
+        // }
+        // BaseTool bt = new BaseTool();
+        WorkflowTool wt = BeanTool.getBean(WorkflowTool.class);
 
         if (BaseTool.isNull(historyId)) {
             historyId = new RandomString(18).nextString();
+            wt.execute(historyId, workflowId, "1", hostStrings, password, envs, "");
         }
         
 
